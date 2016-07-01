7b09d9a7a272e69503113cacf14764d9a00174ad
Tina Parija <tinaparija@gmail.com>
2016-06-30 17:44:59 -0700

    yey

--- javascript/myjquery.js
+++ javascript/myjquery.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
-
+$('a').click(function(){
+    $('html, body').animate({
+        scrollTop: $('[name="' + $.attr(this, 'href').substr(1) + '"]').offset().top
+    }, 500);
+    return false;
+});
 });
